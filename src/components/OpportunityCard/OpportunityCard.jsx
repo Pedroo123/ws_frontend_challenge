8d97fe418037b0c13db93cb3f8c0f5cbbb44441f
Pedroo123 <pedroaugusto466@gmail.com>
2020-06-22 08:02:56 -0300

    Isolating the logic to render data

--- src/components/OpportunityCard/OpportunityCard.jsx
+++ src/components/OpportunityCard/OpportunityCard.jsx
@@ -3,6 +3,7 @@ import Card from '../Card/Card';
 import { API_URL } from '../../config';
 import { responseHandle } from '../Helpers/helpers'; 
 import Loading from '../Loading/Loading';
+import './OpportunityCard.css';
 
 export default class OpportunityCard extends React.Component {
 
@@ -37,23 +38,34 @@ export default class OpportunityCard extends React.Component {
         const mappedOpportunities = opportunities.map((items) => 
         <Card>
             <h3>Opportunities (12)</h3>
-            <div key={items.won.id} className="Opportunities-won">
-                <span  className="Opportunities-won-total">{items.won.totalWonOpportunities}</span>
-                <span className="Opportunities-won-value">{items.won.totalValue}</span>
-            </div>
-            
-            <div key={items.lost.id} className="Opportunities-lost">
-                <span className="Opportunities-lost-total">{items.lost.totalLostOpportunities}</span>
-                <span className="Opportunities-lost-value">{items.lost.totalLostValue}</span>
-            </div>
+            <div className="opportunities-container">
+                <div key={items.won.id} className="Opportunities-won">
+                    <div className="square-won">
+                        <span className="Opportunities-won-total">{items.won.totalWonOpportunities}</span>
+                    </div>
+                    <span className="Opportunities-won-value">R$: {items.won.totalValue}</span>
+                </div>
+                
+                <div key={items.lost.id} className="Opportunities-lost">
+                    <div className="square-lost">
+                        <span className="Opportunities-lost-total">{items.lost.totalLostOpportunities}</span>
+                    </div>
+                    <span className="Opportunities-lost-value">R$: {items.lost.totalLostValue}</span>
+                </div>
 
-            <div key={items.open.id} className="Opportunities-open">
-                <span key={items.open.id} className="Opportunities-open-total">{items.open.totalOpenOpportunities}</span>
-                <span className="Opportunities-open-value">{items.open.totalOpenValue}</span>
-            </div>
+                <div key={items.open.id} className="Opportunities-open">
+                    <div className="square-open">
+                        <span key={items.open.id} className="Opportunities-open-total">{items.open.totalOpenOpportunities}</span>
+                    </div>
+                    <span className="Opportunities-open-value">R$: {items.open.totalOpenValue}</span>
+                </div>
 
-            <div key={items.canceled.id} className="Opportunities-canceled">
-                <span key={items.canceled.id} className="Opportunities-canceled-total">{items.canceled.totalCanceledOpportunities}</span>
+                <div key={items.canceled.id} className="Opportunities-canceled">
+                    <div className="square-canceled">
+                        <span key={items.canceled.id} className="Opportunities-canceled-total">{items.canceled.totalCanceledOpportunities}</span>
+                    </div>
+                    <span className="opportunities-canceled-value">R$: {items.canceled.totalCanceledValue}</span>
+                </div>
             </div>
         </Card>
         );
