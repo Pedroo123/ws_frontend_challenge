08d6d09ec1ef77ed96b3b98c8a77ea7a728167c7
Pedroo123 <pedroaugusto466@gmail.com>
2020-06-22 08:09:51 -0300

    Add the timeline component as a children on card

--- src/components/ActivitiesCard/ActivitiesCard.jsx
+++ src/components/ActivitiesCard/ActivitiesCard.jsx
@@ -4,6 +4,8 @@ import { responseHandle } from '../Helpers/helpers';
 import Search from '../Search/Search';
 import Card from '../Card/Card';
 import Loading from '../Loading/Loading';
+import Timeline from '../Timeline/Timeline';
+import './ActivitiesCard.css';
 
 export default class ActivitiesCard extends React.Component {
 
@@ -36,25 +38,45 @@ export default class ActivitiesCard extends React.Component {
         const { activities } = this.state;
 
         const mappedActivities = activities.map((items) => 
-            <Card>
-                <h3>Atividades</h3>
+            <Card width="500px" height="760px">
+                <h3>Activities</h3>
                 <div className="search-container">
                     <Search />                   
                 </div>
-                <div>
-                    Total: {items.total.totalActivities}
+                <div className="activities-container">
+                    <div className="activities-total">
+                        <div className="square-total">
+                            <span className="activities-total-number">{items.total.totalActivities}</span>
+                        </div>
+                        <span className="activities-total-text">Total</span>
+                    </div>
+                    <div className="activities-late">
+                        <div className="square-late">
+                            <span className="activities-late-number">{items.late.totalLateActivities}</span>
+                        </div>
+                        <span className="activities-late-text">Late</span>
+                    </div>
+                    <div className="activities-in-progress">
+                        <div className="square-in-progress">
+                            <span className="activities-in-progress-number">{items.inProgress.totalActivitiesInProgress}</span>
+                        </div>
+                        <span className="activities-in-progress-text">In Progress</span>
+                    </div>
+                    <div className="activities-waiting">
+                        <div className="square-waiting">
+                            <span className="activities-waiting-number">{items.waiting.totalWaitingActivities}</span>
+                        </div>
+                        <span className="activities-waiting-text">Waiting</span>
+                    </div>
+                    <div className="activities-completed">
+                        <div className="square-completed">
+                            <span className="activities-completed-number">{items.completed.totalCompletedActivities}</span>
+                        </div>
+                        <span className="activities-completed-text">Completed</span>
+                    </div>
                 </div>
-                <div className="Activities-late">
-                    Late: {items.late.totalLateActivities}
-                </div>
-                <div className="Activities-in-progress">
-                    In Progress: {items.inProgress.totalActivitiesInProgress}
-                </div>
-                <div className="Activities-waiting">
-                    Waiting: {items.waiting.totalWaitingActivities}
-                </div>
-                <div className="Activities-completed">
-                    Completed: {items.completed.totalCompletedActivities}
+                <div className="timeline-container">
+                        <Timeline />
                 </div>
             </Card>
         );
