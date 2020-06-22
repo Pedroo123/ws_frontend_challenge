9f004d3419616f126af1853885fffde59f16efae
Pedroo123 <pedroaugusto466@gmail.com>
2020-06-22 08:03:54 -0300

    Isolating the logic to render data

--- src/components/FinancialTitlesCard/FinancialTitlesCard.jsx
+++ src/components/FinancialTitlesCard/FinancialTitlesCard.jsx
@@ -3,6 +3,7 @@ import Loading from '../Loading/Loading';
 import Card from '../Card/Card';
 import { API_URL } from '../../config';
 import { responseHandle } from '../Helpers/helpers';
+import './FinancialTitlesCard.css';
 
 export default class FinancialTitlesCard extends React.Component {
 
@@ -34,20 +35,31 @@ export default class FinancialTitlesCard extends React.Component {
         const mappedtTitulos = titulos.map((items) =>
             <Card>
                 <h3>Financial Titles</h3>
-                <div className="Vencidos">
-                        <span className="Financial-titles-vencidos-total">{items.vencidos.totalVencidos}</span>
-                        <span className="Financial-titles-vencidos-value">{items.vencidos.totalVencidosValue}</span>
+                <div className="financial-titles-container">
+                    <div className="Vencidos">
+                        <div className="square-vencidos">
+                            <span className="Financial-titles-vencidos-total">{items.vencidos.totalVencidos}</span>
+                        </div>
+                        <span className="Financial-titles-vencidos-value">R$: {items.vencidos.totalVencidosValue}</span>
+                        <span className="financial-titles-vencidos-text">Expired</span>
                     </div>
 
                     <div className="Para-vencer">
-                        <span className="Financial-titles-para-vencer-total">{items.paraVencer.totalParaVencer}</span>
-                        <span className="Financial-titles-para-vencer-value">{items.paraVencer.totalParaVencerValue}</span>
+                        <div className="square-para-vencer">
+                            <span className="Financial-titles-para-vencer-total">{items.paraVencer.totalParaVencer}</span>
+                        </div>
+                        <span className="Financial-titles-para-vencer-value">R$: {items.paraVencer.totalParaVencerValue}</span>
+                        <span className="financial-titles-para-vencer-text">To expire</span>
                     </div>
 
                     <div className="Pagos">
-                        <span className="Financial-titles-pagos-total">{items.pagos.totalPagos}</span>
-                        <span className="Financial-titles-pagos-value">{items.pagos.totalPagosValue}</span>
+                        <div className="square-pagos">
+                            <span className="Financial-titles-pagos-total">{items.pagos.totalPagos}</span>
+                        </div>
+                        <span className="Financial-titles-pagos-value">R$: {items.pagos.totalPagosValue}</span>
+                        <span className="financial-titles-pagos-text">Paid</span>
                     </div>
+                </div>
             </Card>
         );
 
